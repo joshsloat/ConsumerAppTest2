3c01ac18e839075137963e228c1dd522a95813c9
Josh Sloat <jsloat@articulate.com>
2013-04-02 09:30:48 -0400

    Initial commit of app project. Also linked app project up with static lib.

--- ConsumerAppTest/ViewController.m
+++ ConsumerAppTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <StaticLibTest/CLStaticLibTest.h>
 
 @interface ViewController ()
 
@@ -18,6 +19,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CLStaticLibTest *libTest = [[CLStaticLibTest alloc] init];
+    [libTest testMethod];
 }
 
 - (void)didReceiveMemoryWarning
